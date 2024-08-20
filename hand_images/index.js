0317f2b7e180b86691b09e04f468309f8727aec1
cluracan1 <cluracan1@gmail.com>
2024-08-20 09:01:19 +0100

    index svgs

---
+++ hand_images/index.js
@@ -0,0 +1,25 @@
+import light_0 from "./light-0.svg";
+import light_1 from "./light-1.svg";
+import light_2 from "./light-2.svg";
+import light_3 from "./light-3.svg";
+import light_4 from "./light-4.svg";
+import dark_0 from "./dark-0.svg";
+import dark_1 from "./dark-1.svg";
+import dark_2 from "./dark-2.svg";
+import dark_3 from "./dark-3.svg";
+import dark_4 from "./dark-4.svg";
+import selected_0 from "./selected-0.svg";
+import selected_1 from "./selected-1.svg";
+import selected_2 from "./selected-2.svg";
+import selected_3 from "./selected-3.svg";
+import selected_4 from "./selected-4.svg";
+const handImagesLight = [light_0, light_1, light_2, light_3, light_4];
+const handImagesDark = [dark_0, dark_1, dark_2, dark_3, dark_4];
+const handImagesSelected = [
+  selected_0,
+  selected_1,
+  selected_2,
+  selected_3,
+  selected_4,
+];
+export { handImagesLight, handImagesDark, handImagesSelected };
